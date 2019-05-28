048180129723744c9b5207b987cd35b06cbcb968
Ibabalola <info@isaacbabalola.co.uk>
2019-05-29 00:44:25 +0100

    dev environment set up

---
+++ webpack.base.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = env => {
+    const {PLATFORM, VERSION} = env;
+    return merge([
+        {
+            entry: {
+                app: './src/main.js'
+            },
+            output: {
+                filename: '[name].bundle.js',
+                path: path.resolve(__dirname, 'dist')
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        use: {
+                            loader: 'babel-loader'
+                        }
+                    },
+                    {
+                        test: /\.(scss|css)$/,
+                        use: [
+                            PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+                            'css-loader',
+                            'sass-loader'
+                        ]
+                    }
+                ]
+            },
+            plugins: [
+                new HtmlWebpackPlugin({
+                    template: './src/index.html',
+                    filename: './index.html'
+                }),
+                new webpack.DefinePlugin({
+                    'process.env.VERSION': JSON.stringify(env.VERSION),
+                    'process.env.PLATFORM': JSON.stringify(env.PLATFORM)
+                }),
+                new CopyWebpackPlugin([
+                    {from: 'static'}
+                ])
+            ]
+        }
+    ])
+};
+
+
+
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,26 +1,41 @@
 const merge = require('webpack-merge');
-const commons = require('./webpack.common');
 
+// Config
+const commons = require('./webpack.base');
+
+// Plugins
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const Visualizer = require('webpack-visualizer-plugin');
 
-const productionConfiguration = function (env) {
-    return {
-        optimization: {
-            minimizer: [
-                new UglifyJsPlugin({
-                    uglifyOptions: {
-                        mangle: {
-                            keep_fnames: true,
-                        },
-                    },
-                })
-            ],
-        },
-        plugins: [
-            new OptimizeCssAssetsPlugin(),
-        ]
-    };
+const productionConfiguration = env => {
+    return merge([
+        {
+            optimization: {
+                // runtimeChunk: 'single',
+                // splitChunks: {
+                //   cacheGroups: {
+                //     vendor: {
+                //       test: /[\\/]node_modules[\\/]/,
+                //       name: 'vendors',
+                //       chunks: 'all'
+                //     }
+                //   }
+                // },
+                minimizer: [new UglifyJsPlugin()],
+            },
+            plugins: [
+                new MiniCssExtractPlugin({
+                    filename: 'styles.css',
+                }),
+                new OptimizeCssAssetsPlugin(),
+                new Visualizer({ filename: './statistics.html' })
+            ]
+        }
+    ]);
 };
 
-module.exports = merge(commons, productionConfiguration);
+module.exports = env => {
+    return merge(commons(env), productionConfiguration(env));
+};
